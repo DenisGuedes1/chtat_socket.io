92fe5c2af3a0f3b76c1e6116716f7daf26711660
DenisGuedes1 <denisguedesmoraes@gmail.com>
2023-09-13 14:29:54 -0300

    add file and env

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { usuarioEntrarSala, getUsuariosSala, mensagemFormatada, getUsuario, usuar
 
 const app = express();
 const server = http.createServer(app);
-const PORT = 4000;
+const PORT = process.env.PORT;
 const io = socketIO(server);
 
 
